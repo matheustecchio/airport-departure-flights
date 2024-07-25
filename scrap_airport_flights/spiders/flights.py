fbae3b8e89b9eea90be005d1cd6593b6430855d7
Matheus Tecchio <matheusdiastecchio@gmail.com>
2024-07-25 18:12:26 +0100

    refactoring variable names and add more header information

--- scrap_airport_flights/spiders/flights.py
+++ scrap_airport_flights/spiders/flights.py
@@ -8,22 +8,24 @@ class FlightsSpider(scrapy.Spider):
     start_urls      = ["https://www.flightaware.com/live/airport/EICK"]
 
     def parse(self, response):
-        departure_times:list = response.css("#scheduled-board td:nth-child(4) i::text").extract()
-        aircraft_models:list = response.css("#scheduled-board .flight-ident+ td a::text").extract()
-        airport_IATAs:list   = response.css("#scheduled-board .hint+ span a::text").extract()
-        airport_names:list   = response.css("#scheduled-board .hint span::text").extract()
-        flight_numbers:list  = response.css("#scheduled-board .flight-ident a::text").extract()
-        flight_quantity:int  = len(flight_numbers)
+        departure_times:list    = response.css("#scheduled-board td:nth-child(4) i::text").extract()
+        aircraft_models:list    = response.css("#scheduled-board .flight-ident+ td a::text").extract()
+        destination_IATAs:list  = response.css("#scheduled-board .hint+ span a::text").extract()
+        destination_names:list  = response.css("#scheduled-board .hint span::text").extract()
+        flight_numbers:list     = response.css("#scheduled-board .flight-ident a::text").extract()
+        flight_quantity:int     = len(flight_numbers)
         
         yield {
-            "Date": time.strftime("%Y-%m-%d %H:%M:%S")
+            "Date": time.strftime("%Y-%m-%d"),
+            "Time": time.strftime("%H:%M"),
+            "Airport Name": response.css(".airportTrackerTitleText::text").get(),
             }
         
         for flight in range(flight_quantity):
             yield {
                 "Departure Time": departure_times[flight][0:5],
                 "Aircraft Model": aircraft_models[flight],
-                "Airport IATA": airport_IATAs[flight],
-                "Airport Name": airport_names[flight],
-                "Flight Number": flight_numbers[flight]
-            } 
+                "Destination IATA": destination_IATAs[flight],
+                "Destination Name": destination_names[flight],
+                "Flight Number": flight_numbers[flight],
+            }
