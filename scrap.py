6aa18433220b30e18411fca5b300cd21c2896ead
Matheus Tecchio <matheusdiastecchio@gmail.com>
2024-08-01 19:06:08 +0100

    fix scrap.py on Windows and add error handling

--- scrap.py
+++ scrap.py
@@ -1,15 +1,21 @@
 import subprocess
 import platform
 
-script = """
+
+windows_script = """
+scrapy runspider .\\scrap_airport_flights\\spiders\\flights.py -O flights.csv
+"""
+unix_script = """
 scrapy runspider scrap_airport_flights/spiders/flights.py -O flights.csv
 """
 
-if platform.system() == "Windows":
-    process = subprocess.run(["powershell", "-Command", script], capture_output=True, text=True)
-else:
-    process = subprocess.run(script, shell=True, capture_output=True, text=True)
-    
-print(process.stdout)
-print(process.stderr)
-print("Data scrapping completed successfully!")
+try:
+    if platform.system() == "Windows":
+        process = subprocess.run(["powershell", "-Command", windows_script], capture_output=True, text=True)
+    else:
+        process = subprocess.run(unix_script, shell=True, capture_output=True, text=True)
+except:
+    print("An error occurred while executing the scraping script.")
+    exit(1)  
+      
+print("\nData scrapping completed successfully!\n")
