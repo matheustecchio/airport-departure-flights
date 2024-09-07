290521aca4496a80f00f201353632ae2072fa1e2
Matheus Tecchio <52295230+matheustecchio@users.noreply.github.com>
2024-09-07 16:46:08 +0100

    reduce repeated code (#19)
    * reduce repeated code
    
    * no more multiple directories in cmd/ for each airport
    * cmd/ much simpler and easer to refactor/fix any bug
    
    * update documentation

--- cmd/main.py
+++ cmd/main.py
@@ -1,11 +1,35 @@
-from cork.main import main as cork_main
-from dublin.main import main as dublin_main
-from shannon.main import main as shannon_main
+from process_data import process_data
+from scrap import scrap
+from convert_csv_to_html import convert_csv_to_html
+
+airport_list = ["cork", "dublin", "shannon"]
 
 def main():
-    cork_main()
-    dublin_main()
-    shannon_main()
+    for airport in airport_list:
+        try:
+            scrap(airport)
+        except Exception as e:
+            print(f"Error occurred while scraping {airport}: {str(e)}")
+            exit(1)
+        else:
+            print(f"Data scrapping completed successfully for {airport}.")
+        
+        try:
+            process_data(airport)
+        except Exception as e:
+            print(f"Error occurred while processing {airport}: {str(e)}")
+            exit(1)
+        else:
+            print(f"Data processing completed successfully for {airport}.")
+        
+        try:
+            convert_csv_to_html(airport)
+        except Exception as e:
+            print(f"Error occurred while converting {airport} data to HTML: {str(e)}")
+            exit(1)
+        else:
+            print(f"Data conversion to HTML completed successfully for {airport}.")
 
 if __name__ == "__main__":
     main()
+
